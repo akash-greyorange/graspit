c700dd16e8a2e3de3f4382fd570609c91aa754c8
root <akash.p@greyorange.sg>
2017-09-22 20:02:27 +0530

    Added Roll Penalty with Yaw inputs

--- src/EGPlanner/energy/searchEnergy.cpp
+++ src/EGPlanner/energy/searchEnergy.cpp
@@ -61,6 +61,9 @@ PROF_DECLARE(QS);
 #define HAND_POSE_PITCH_FILTER_UPPER                         (1.0)          /* 60 degrees cone up and down */
 #define HAND_POSE_PITCH_FILTER_LOWER                         (-1.0)
 
+#define HAND_POSE_ROLL_FILTER_UPPER                         (1.0)          /* 60 degrees cone up and down */
+#define HAND_POSE_ROLL_FILTER_LOWER                         (-1.0)
+
 //todo move this out of here
 const double unbalancedForceThreshold = 1.0e10;
 
@@ -199,20 +202,15 @@ void SearchEnergy::analyzeState(bool &isLegal, double &stateEnergy, const GraspP
     bool grasp_out_of_limit = false ;
     bool grasp_x_axis_exceeded = false ;
     bool grasp_pitch_exceeded = false ;
-    double position_violation_penalty = 0 , pitch_violation_penalty = 0 ;
+    bool grasp_roll_exceeded = false ;
+    double position_violation_penalty = 0 , pitch_violation_penalty = 0 , roll_violation_penalty = 0 ;
 
-    double hand_pitch_upper_limit , hand_pitch_lower_limit ;
+    double hand_pitch_upper_limit , hand_pitch_lower_limit , hand_roll_upper_limit , hand_roll_lower_limit ;
 
-    if(hand_yaw > 0)
-    {
-        hand_pitch_upper_limit = HAND_POSE_PITCH_FILTER_UPPER + hand_yaw ;
-        hand_pitch_lower_limit = HAND_POSE_PITCH_FILTER_UPPER - hand_yaw ;
-    }
-    else
-    {
-        hand_pitch_upper_limit = HAND_POSE_PITCH_FILTER_UPPER + hand_yaw ;
-        hand_pitch_lower_limit = HAND_POSE_PITCH_FILTER_UPPER - hand_yaw ;
-    }
+    hand_pitch_upper_limit = HAND_POSE_PITCH_FILTER_UPPER + hand_yaw ;
+    hand_pitch_lower_limit = HAND_POSE_PITCH_FILTER_UPPER - hand_yaw ;
+    hand_roll_upper_limit = HAND_POSE_ROLL_FILTER_UPPER + hand_yaw ;
+    hand_roll_lower_limit = HAND_POSE_ROLL_FILTER_LOWER - hand_yaw ;
 
     if(hand_translation.x() > object_translation.x())
     {
@@ -232,6 +230,20 @@ void SearchEnergy::analyzeState(bool &isLegal, double &stateEnergy, const GraspP
         else
         {
             pitch_violation_penalty = ((hand_pitch_upper_limit*2) + hand_pitch) * 10 ;
+        }
+    }
+
+    if(!((hand_roll >= hand_roll_lower_limit) && (hand_roll <= hand_roll_upper_limit)))
+    {
+        grasp_out_of_limit = true ;
+        grasp_roll_exceeded = true ;
+        if(hand_roll >= 0)
+        {
+            pitch_violation_penalty = ((hand_roll_upper_limit*2) - hand_roll) * 10 ;
+        }
+        else
+        {
+            pitch_violation_penalty = ((hand_roll_upper_limit*2) + hand_roll) * 10 ;
         }
     }
 
