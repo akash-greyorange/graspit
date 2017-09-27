9825ab8828971bf50ddc24abf2125d91be24ae2e
root <akash.p@greyorange.sg>
2017-09-27 11:11:30 +0530

    RPY Checks order changed

--- src/EGPlanner/energy/searchEnergy.cpp
+++ src/EGPlanner/energy/searchEnergy.cpp
@@ -244,50 +244,54 @@ void SearchEnergy::analyzeState(bool &isLegal, double &stateEnergy, const GraspP
         grasp_x_axis_exceeded = true ;
         position_violation_penalty = (hand_translation.x() - object_translation.x()) * 100 ;
     }
+    else
+    {
+        if(!(((hand_yaw >= HAND_POSE_YAW_RANGE_1_LOWER) && (hand_yaw <= HAND_POSE_YAW_RANGE_1_UPPER)) || ((hand_yaw >= HAND_POSE_YAW_RANGE_2_LOWER) &&
+            (hand_yaw <= HAND_POSE_YAW_RANGE_2_UPPER)) || ((hand_yaw >= HAND_POSE_YAW_RANGE_3_LOWER) && (hand_yaw <= HAND_POSE_YAW_RANGE_3_UPPER))))
+        {
+            grasp_out_of_limit = true ;
+            grasp_yaw_exceeded = true ;
+            if(hand_yaw >= 0)
+            {
+                yaw_violation_penalty = hand_yaw * 100 ;
+            }
+            else
+            {
+                yaw_violation_penalty -= hand_yaw * 100 ;
+            }
+        }
+        else
+        {
+            if(!(((hand_pitch >= HAND_POSE_PITCH_RANGE_1_LOWER) && (hand_pitch <= HAND_POSE_PITCH_RANGE_1_UPPER)) || ((hand_pitch >= HAND_POSE_PITCH_RANGE_2_LOWER) &&
+                (hand_pitch <= HAND_POSE_PITCH_RANGE_2_UPPER))))
+            {
+                grasp_out_of_limit = true ;
+                grasp_pitch_exceeded = true ;
+                if(hand_pitch >= 0)
+                {
+                    pitch_violation_penalty = (1.57 - hand_pitch) * 100 ;
+                }
+                else
+                {
+                    pitch_violation_penalty = (1.57 + hand_pitch) * 100 ;
+                }
+            }
 
-    if(!(((hand_pitch >= HAND_POSE_PITCH_RANGE_1_LOWER) && (hand_pitch <= HAND_POSE_PITCH_RANGE_1_UPPER)) || ((hand_pitch >= HAND_POSE_PITCH_RANGE_2_LOWER) &&
-        (hand_pitch <= HAND_POSE_PITCH_RANGE_2_UPPER))))
-    {
-        grasp_out_of_limit = true ;
-        grasp_pitch_exceeded = true ;
-        if(hand_pitch >= 0)
-        {
-            pitch_violation_penalty = (1.57 - hand_pitch) * 10 ;
-        }
-        else
-        {
-            pitch_violation_penalty = (1.57 + hand_pitch) * 10 ;
-        }
-    }
-    
-    if(!(((hand_roll >= HAND_POSE_ROLL_RANGE_1_LOWER) && (hand_roll <= HAND_POSE_ROLL_RANGE_1_UPPER)) || ((hand_roll >= HAND_POSE_ROLL_RANGE_2_LOWER) && 
-        (hand_roll <= HAND_POSE_ROLL_RANGE_2_UPPER)) || ((hand_roll >= HAND_POSE_ROLL_RANGE_3_LOWER) && (hand_roll <= HAND_POSE_ROLL_RANGE_3_UPPER))
-        || ((hand_roll >= HAND_POSE_ROLL_RANGE_4_LOWER) && (hand_roll <= HAND_POSE_ROLL_RANGE_4_UPPER))))
-    {
-        grasp_out_of_limit = true ;
-        grasp_roll_exceeded = true ;
-        if(hand_roll >= 0)
-        {
-            roll_violation_penalty = (hand_roll + 0.4) * 100 ;
-        }
-        else
-        {
-            roll_violation_penalty -= (hand_roll - 0.4) * 100 ;
-        }
-    }
-
-    if(!(((hand_yaw >= HAND_POSE_YAW_RANGE_1_LOWER) && (hand_yaw <= HAND_POSE_YAW_RANGE_1_UPPER)) || ((hand_yaw >= HAND_POSE_YAW_RANGE_2_LOWER) &&
-        (hand_yaw <= HAND_POSE_YAW_RANGE_2_UPPER)) || ((hand_yaw >= HAND_POSE_YAW_RANGE_3_LOWER) && (hand_yaw <= HAND_POSE_YAW_RANGE_3_UPPER))))
-    {
-        grasp_out_of_limit = true ;
-        grasp_yaw_exceeded = true ;
-        if(hand_yaw >= 0)
-        {
-            yaw_violation_penalty = hand_yaw * 100 ;
-        }
-        else
-        {
-            yaw_violation_penalty -= hand_yaw * 100 ;
+            if(!(((hand_roll >= HAND_POSE_ROLL_RANGE_1_LOWER) && (hand_roll <= HAND_POSE_ROLL_RANGE_1_UPPER)) || ((hand_roll >= HAND_POSE_ROLL_RANGE_2_LOWER) && 
+                (hand_roll <= HAND_POSE_ROLL_RANGE_2_UPPER)) || ((hand_roll >= HAND_POSE_ROLL_RANGE_3_LOWER) && (hand_roll <= HAND_POSE_ROLL_RANGE_3_UPPER))
+                || ((hand_roll >= HAND_POSE_ROLL_RANGE_4_LOWER) && (hand_roll <= HAND_POSE_ROLL_RANGE_4_UPPER))))
+            {
+                grasp_out_of_limit = true ;
+                grasp_roll_exceeded = true ;
+                if(hand_roll >= 0)
+                {
+                    roll_violation_penalty = (hand_roll + 0.4) * 100 ;
+                }
+                else
+                {
+                    roll_violation_penalty -= (hand_roll - 0.4) * 100 ;
+                }
+            }
         }
     }
 
