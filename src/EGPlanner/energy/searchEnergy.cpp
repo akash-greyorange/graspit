e441f4069ee7e8a7dc73c48efa9fd901ff306e24
root <akash.p@greyorange.sg>
2017-09-28 13:13:17 +0530

    Checking Pitch and Roll only

--- src/EGPlanner/energy/searchEnergy.cpp
+++ src/EGPlanner/energy/searchEnergy.cpp
@@ -302,7 +302,7 @@ void SearchEnergy::analyzeState(bool &isLegal, double &stateEnergy, const GraspP
             }
         }
         else
-        {
+        
             /*if(!(((hand_yaw >= HAND_POSE_YAW_RANGE_1_LOWER) && (hand_yaw <= HAND_POSE_YAW_RANGE_1_UPPER)) || ((hand_yaw >= HAND_POSE_YAW_RANGE_2_LOWER) &&
             (hand_yaw <= HAND_POSE_YAW_RANGE_2_UPPER)) || ((hand_yaw >= HAND_POSE_YAW_RANGE_3_LOWER) && (hand_yaw <= HAND_POSE_YAW_RANGE_3_UPPER)) ||
             ((hand_yaw >= HAND_POSE_YAW_RANGE_4_LOWER) && (hand_yaw <= HAND_POSE_YAW_RANGE_4_UPPER))))
@@ -317,7 +317,7 @@ void SearchEnergy::analyzeState(bool &isLegal, double &stateEnergy, const GraspP
                 {
                     yaw_violation_penalty -= hand_yaw * 100 ;
                 }
-            }
+            }*/
 
             if(!(((hand_roll >= HAND_POSE_ROLL_RANGE_1_LOWER) && (hand_roll <= HAND_POSE_ROLL_RANGE_1_UPPER)) || ((hand_roll >= HAND_POSE_ROLL_RANGE_2_LOWER) && 
                 (hand_roll <= HAND_POSE_ROLL_RANGE_2_UPPER)) || ((hand_roll >= HAND_POSE_ROLL_RANGE_3_LOWER) && (hand_roll <= HAND_POSE_ROLL_RANGE_3_UPPER))
@@ -333,7 +333,7 @@ void SearchEnergy::analyzeState(bool &isLegal, double &stateEnergy, const GraspP
                 {
                     roll_violation_penalty -= (hand_roll) * 100 ;
                 }
-            }*/
+            }
         }
     }
 
