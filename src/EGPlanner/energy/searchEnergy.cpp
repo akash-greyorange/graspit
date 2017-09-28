875d254e30c74b03255dd0a01da32efb8323ab7b
root <akash.p@greyorange.sg>
2017-09-28 20:09:22 +0530

    WIP

--- src/EGPlanner/energy/searchEnergy.cpp
+++ src/EGPlanner/energy/searchEnergy.cpp
@@ -295,7 +295,7 @@ void SearchEnergy::analyzeState(bool &isLegal, double &stateEnergy, const GraspP
     
     if(hand_translation.x() > (object_translation.x() - 150))
     {
-        grasp_out_of_limit = true 
+        grasp_out_of_limit = true ;
         grasp_x_axis_exceeded = true ;
         position_violation_penalty = (hand_translation.x() - object_translation.x()) * 100 ;
     }
